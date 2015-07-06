fd973f6ff53dc7b19880f5b03e2513ebe1aeda94
noboru-i <ishikura.noboru@gmail.com>
2015-07-06 17:59:42 +0900

    I forget ;

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -2,14 +2,14 @@
 
 gem install --no-document checkstyle_filter-git saddler saddler-reporter-github findbugs_translate_checkstyle_format android_lint_translate_checkstyle_format
 
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ]; then
     echo 'Failed to install gems.'
     exit 1
 fi
 
 ./gradlew check
 
-if [ $? -ne 0 ] then
+if [ $? -ne 0 ]; then
     echo 'Failed gradle check task.'
     exit 1
 fi
