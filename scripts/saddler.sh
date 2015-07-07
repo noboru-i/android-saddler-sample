d6168f39174f9fe82846fa6a0aa1c607cba1c019
noboru-i <ishikura.noboru@gmail.com>
2015-07-07 12:46:13 +0900

    switch reporter

--- scripts/saddler.sh
+++ scripts/saddler.sh
@@ -33,7 +33,9 @@ cp -v "app/build/outputs/lint-results.xml" "$LINT_RESULT_DIR/"
 
 if [ -z "${CI_PULL_REQUEST}" ]; then
     # when not pull request
-    exit 0
+    REPORTER=Saddler::Reporter::Github::PullRequestReviewComment
+else
+    REPORTER=Saddler::Reporter::Github::CommitReviewComment
 fi
 
 echo "********************"
@@ -41,7 +43,7 @@ echo "* checkstyle       *"
 echo "********************"
 cat app/build/reports/checkstyle/checkstyle.xml \
     | checkstyle_filter-git diff origin/master \
-    | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
 
 echo "********************"
 echo "* findbugs         *"
@@ -49,7 +51,7 @@ echo "********************"
 cat app/build/reports/findbugs/findbugs.xml \
     | findbugs_translate_checkstyle_format translate \
     | checkstyle_filter-git diff origin/master \
-    | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
 
 echo "********************"
 echo "* android lint     *"
@@ -57,4 +59,4 @@ echo "********************"
 cat app/build/outputs/lint-results.xml \
     | android_lint_translate_checkstyle_format translate \
     | checkstyle_filter-git diff origin/master \
-    | saddler report --require saddler/reporter/github --reporter Saddler::Reporter::Github::PullRequestReviewComment
+    | saddler report --require saddler/reporter/github --reporter $REPORTER
