39bda15cba900c7267ff19890f163e714d638058
noboru-i <ishikura.noboru@gmail.com>
2015-09-01 19:20:23 +0900

    add findbug failure

--- app/src/main/java/hm/orz/chaos114/android/androidsaddlersample/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/androidsaddlersample/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            new IllegalArgumentException("input must not be null");
+        }
     }
 
     @Override
