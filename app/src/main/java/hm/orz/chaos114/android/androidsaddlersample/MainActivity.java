5315f9ae578a1835058ef6b6632e634aea105e1d
noboru-i <ishikura.noboru@gmail.com>
2015-11-18 23:55:00 +0900

    delete unused

--- app/src/main/java/hm/orz/chaos114/android/androidsaddlersample/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/androidsaddlersample/MainActivity.java
@@ -1,24 +1,16 @@
 package hm.orz.chaos114.android.androidsaddlersample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean mFoo;
-    private boolean BAR;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        for (int i = 0; i < 10; i++) {
-            Log.d("FOO", "test");
-        }
     }
 
     @Override
